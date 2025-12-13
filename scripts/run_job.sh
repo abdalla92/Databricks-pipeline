094dda456ea5102dfe565b4dc8eeb9212be8a2aa
abdalla92 <abdalla-92@hotmail.com>
2025-12-13 18:18:31 -0500

    creation

--- scripts/run_job.sh
+++ scripts/run_job.sh
@@ -7,7 +7,7 @@ DATABRICKS_URL= "https://<your-databricks-instance>"
 DATABRICKS_TOKEN="<your-databricks-token>"
 
 # Specify the job ID or job name to run
-JOB_ID="57fdf3fb81ac0409" #"<your-job-id>"
+JOB_ID="<your-job-id>"
 
 # Execute the job using the Databricks REST API
 curl -X POST "$DATABRICKS_URL/api/2.0/jobs/run-now" \
